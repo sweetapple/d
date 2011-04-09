0ff1958f9526bc351f28c244e662b5bca54c2203
Peter Wang <happypeter1983@gmail.com>
2011-04-09 15:09:58 +0800

    5

--- digui.c
+++ digui.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 void hanoi (char a, char b, char c, int n);
+static int count;
 int main(int argc, const char *argv[])
 {
+    count=0;
     int n=0;
     printf("please input the number:\n");
     scanf ("%d",&n);
     hanoi ('A','B','C',n);
+    printf("%d\n",count);
     return 0;
 }
 void hanoi (char a, char b, char c, int n)
 {
     if (n==1) 
     {
-        printf("%c---->%c\n",a,c);
+    count++;
+        printf("%c---->%c %d\n",a,c,count);
         return ;
     }
     hanoi (a,c,b,n-1);
-    printf("%c---->%c\n",a,c);
+    count++;
+    printf("%c---->%c %d\n",a,c,count);
     hanoi (b, a, c,n-1);
 }
