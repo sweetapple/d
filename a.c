c06a5f10b64da7e234bc72d79eac75f611be3d1b
Peter Wang <happypeter1983@gmail.com>
2011-04-09 10:32:55 +0800

    1

---
+++ a.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 100
+    void init_array (int p[],int n);
+    void bubble_array (int p[],int n);
+    void print_array (int p[],int n);
+
+    int main(int argc, const char *argv[])
+    {
+    int array [N];
+    init_array (array,N);
+    printf("original:\n");
+    print_array (array,N);
+    getchar ();
+    printf("sorted:\n");
+    bubble_array (array ,N);
+    print_array (array,N);
+    return 0;
+    }
+    void init_array (int p[],int n)
+    {
+    int i=0;
+    srand (time (NULL) );
+    for (i=0;i<n;i++)
+    {
+        p[i] =(rand()%200);
+    }
+    }
+
+    void print_array (int p[],int n)
+    {
+        int i=0;
+        for (i=0;i<n;i++)
+        {
+            if ( (i%10)==0   )
+            {
+            printf("\n");
+            }
+            printf("%5d",p[i]);
+        }
+        printf("\n");
+    }
+
+    void bubble_array (int p[],int n)
+    {
+        int i=0;
+        int j=0;
+        int temp =0;
+        for (i=0;i<n-1;i++)
+        {
+            for (j = i+1; j < n; j++) 
+            {
+            if (p[i]>p[j]) 
+            {
+                temp=p[i];
+                p[i]=p[j];
+                p[j]=temp;
+            }
+            }
+        }
+    }
