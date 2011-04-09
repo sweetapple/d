ea62e28b40f7c31349348ced7b865a9765a59b17
Peter Wang <happypeter1983@gmail.com>
2011-04-09 16:38:09 +0800

    005

--- zhizhen.c
+++ zhizhen.c
@@ -4,16 +4,19 @@ int main(int argc, const char *argv[])
 {
     char str[20]="This is a C world !";
     char *p=str;
+    char c_str[20];
     printf("%s\n",str);
     getchar();
-    while (*p!='\0')p++;
-    for (p--;p!= (str-1);p-- )
+    char *p_c=c_str;
+    for (p=str;*p!='\0';p++)
     {
         if (*p!=' ')
         {
-        putchar (*p);
+            *p_c=*p;
+            p_c++;
         }
     }
-    putchar ('\n');
+    *p_c='\0';
+    printf("%s\n",c_str);
     return 0;
 }
