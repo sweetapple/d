37c9570ee5bac8e0c782a34c84d2487d43ae44d8
Peter Wang <happypeter1983@gmail.com>
2011-04-10 10:01:54 +0800

    00002

--- shuzu.c
+++ shuzu.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #define N 10
 #define M 10
 int main(int argc, const char *argv[])
@@ -6,20 +8,34 @@ int main(int argc, const char *argv[])
     int array[N][M];
     int i=0;
     int j=0;
-    for (j = 0; j < N; j++) 
+//    for (j = 0; j < N; j++) 
+//    {
+//        for (i = 0; i < M; i++) 
+//        {
+//            array[j][i]=j*M+i+1;
+//        }
+//    }
+//    for
+//    for (j = 0; j < N; j++) 
+//    {
+//        for (i = 0; i < M; i++) 
+//        {
+//            printf("%5d",array[j][i]);
+//        }
+//        printf("\n");
+//    }
+    srand (time (NULL) );
+    for (i = 0; i < M*N; i++) 
     {
-        for (i = 0; i < M; i++) 
-        {
-            array[j][i]=j*M+i+1;
-        }
+        array[0][i]=rand()%(M*N);
     }
-    for (j = 0; j < N; j++) 
+    for (j=0;j<N;j++)
     {
-        for (i = 0; i < M; i++) 
+        for (i=0;i<M;i++)
         {
             printf("%5d",array[j][i]);
         }
-        printf("\n");
     }
+    printf("\n");
     return 0;
 }
