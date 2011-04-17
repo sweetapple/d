511a831347f952197879f0893bb2398f3fb38598
Peter Wang <happypeter1983@gmail.com>
2011-04-17 10:03:12 +0800

    lianbiao 3

--- lianbiao.c
+++ lianbiao.c
@@ -11,9 +11,11 @@ typedef struct node V_NODE;
 int main(int argc, const char *argv[])
 {
     V_NODE stu;
-    stu.number=2;
-    stu.score=150;
-    strcpy (stu.name,"pt");
-    printf("number:%d\tscore:%d\tname:%s\n",stu.number,stu.score,stu.name);
+    V_NODE *ptr=NULL;
+    ptr=&stu;
+    ptr->number=2;
+    ptr->score=150;
+    strcpy (ptr->name,"pt");
+    printf("number:%d\tscore:%d\tname:%s\n",ptr->number,ptr->score,ptr->name);
     return 0;
 }
