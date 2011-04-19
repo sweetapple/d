06a4c6e9d42f28ca44fefa038e6b1cc4efe08ffb
Sweet apple <1543139895@qq.com>
2011-04-19 20:44:21 +0800

    wenjian 3

--- wenjian.c
+++ wenjian.c
@@ -6,17 +6,17 @@ int main(int argc, const char *argv[])
     char str[100]="hello world!";
     char str_1[100];
     int number=78;
-    int r_number=0;
+    int r_number=79;
     fp=fopen("text1","w+");
     if (fp==NULL)
     {
         perror("open:");
         exit(0);
     }
-    fprintf (fp,"%d %d\n",number ,number+1);
+//    fprintf (fp,"%d %d\n",number ,number+1);
 //    fprintf(fp,"%s\n",str);
-//    fscanf (fp,"%s",str_1);
-//    printf("str_1:%s",str_1);
+    fscanf (fp,"%d %d",&number,&r_number);
+    printf("number=%d r_number=%d\n",number,r_number);
     fclose(fp);
     return 0;
 }
