3986f0bd5bfa47773ce7c1594b9bb0ab38b7487e
Sweet apple <1543139895@qq.com>
2011-04-20 19:54:31 +0800

    puts 2

--- puts.c
+++ puts.c
@@ -3,17 +3,20 @@
 int main(int argc, const char *argv[])
 {
     FILE *fp;
-    char s[100]="hello world !";
+    char s[100]="hello world !\n";
     char d[100];
+    gets (d);
+    printf("%s\n",d);
     fp=fopen ("text1","w+");
     if (fp==NULL) 
     {
         perror ("open");
         exit (0);
     }
-    fputs (s,fp);                           //文件写函数
-    rewind (fp);
-    fgets (d,99,fp);                              //文件读函数          
+//    fputs (s,fp);                           //文件写函数
+//    rewind (fp);
+    fgets (d,99,fp);                              //文件读函数 很危险的函数         
     printf("%s\n",d);
+    fclose (fp);
     return 0;
 }
