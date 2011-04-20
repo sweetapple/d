4fa558c33edf8027a1abcffbedd6cb1f7a2f4a77
Sweet apple <1543139895@qq.com>
2011-04-20 20:30:12 +0800

    puts 4

--- puts.c
+++ puts.c
@@ -5,18 +5,18 @@ int main(int argc, const char *argv[])
     FILE *fp;
     char s[100]="hello world !\nThis is a c world!";
     char d[100];
-//    gets (d);
-//    printf("%s\n",d);
-    fp=fopen ("text1","w+");
+    int i=0;
+//    fp=fopen ("text1","r+");                         //覆盖原来的东西，相当于替换
+    fp=fopen ("text1","a+");                           //a+ 累加
     if (fp==NULL) 
     {
         perror ("open");
         exit (0);
     }
-    fputs (s,fp);                           //文件写函数
-    rewind (fp);
-    fgets (d,99,fp);                              //文件读函数 很危险的函数         
-    printf("%s\n",d);
+    while (s[i]!='\0')
+    {
+        putc(s[i++],fp);                            //可输入的东西不受限制，比如\0,\n等等
+    }
     fclose (fp);
     return 0;
 }
