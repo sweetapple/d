fd650e10f107d479ab0ddbf4aa327d8d5b631f0b
Sweet apple <1543139895@qq.com>
2011-04-21 19:22:16 +0800

    fread_fwrite 2

--- fread_fwrite.c
+++ fread_fwrite.c
@@ -5,6 +5,7 @@ int main(int argc, const char *argv[])
     char str[1024];
     FILE *fp1;
     FILE *fp2;
+    int n=0;
     fp1=fopen  ("text1","r+");
     if (fp1==NULL) 
     {
@@ -17,8 +18,12 @@ int main(int argc, const char *argv[])
         perror ("fp2");
         exit (0);
     }
-    fread (str,1,1023,fp1);
-    fwrite (str,1,1023,fp2);
+    n=fread (str,1,1023,fp1);
+    if (n!=0) 
+    {
+        str[n]='\0';
+        fwrite (str,1,n,fp2);
+    }
     fclose (fp1);
     fclose (fp2);
     return 0;
