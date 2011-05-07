859a7cce56c6270e9544a7c0e002ef6c513cd920
Sweet apple <1543139895@qq.com>
2011-05-07 16:38:27 +0800

    luan2  1

---
+++ luan2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+    int a=1,b;
+    for (b = 1; b<=10; b++) 
+    {
+        if(a>=8)
+            break;
+        if(a%2==1)
+        {
+            a+=5;
+//            continue;
+        }
+    a-=3;
+    }
+    printf("%d\n",b);
+    return 0;
+}
